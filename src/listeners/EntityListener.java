d9c74f2d94820f00e7ba0b172198e81521e1fbd0
Jan Strothmann <jan.strothmann@googlemail.com>
2015-11-06 20:29:13 +0100

    Leashes and Mounting Only allows players with pve permission to leash/unleash/mount creatures. Players with access permission can still bind leashed animals to fences, but not unbind them!

--- src/listeners/EntityListener.java
+++ src/listeners/EntityListener.java
@@ -1,5 +1,8 @@
 package listeners;
 
+import org.bukkit.event.player.PlayerUnleashEntityEvent;
+import org.spigotmc.event.entity.EntityMountEvent;
+
 import java.util.Iterator;
 
 import org.bukkit.block.Block;
@@ -8,6 +11,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityUnleashEvent.UnleashReason;
+import org.bukkit.event.entity.PlayerLeashEntityEvent;
 
 import clazz.Area;
 import main.Protector;
@@ -42,6 +47,41 @@ public class EntityListener implements Listener{
 						event.setCancelled(true);
 						return;
 					}
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerUnleashEntity(PlayerUnleashEntityEvent event){
+		if(!event.getReason().equals(UnleashReason.PLAYER_UNLEASH))
+			return;
+		for(Area area : Area.areas){
+			if(area.isInside(event.getEntity().getLocation()) && !area.permission(event.getPlayer().getName(), "pve")){
+				event.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerLeashEntity(PlayerLeashEntityEvent event){
+		for(Area area : Area.areas){
+			if(area.isInside(event.getEntity().getLocation()) && !area.permission(event.getPlayer().getName(), "pve")){
+				event.setCancelled(true);
+			}
+			if(area.isInside(event.getPlayer().getLocation()) && !area.permission(event.getPlayer().getName(), "pve")){
+				event.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onEntityMountEvent(EntityMountEvent event){
+		if(event.getEntity() instanceof Player){
+			Player player = ((Player) event.getEntity());
+			for(Area area : Area.areas){
+				if(area.isInside(event.getMount().getLocation()) && !area.permission(player.getName(), "pve")){
+					event.setCancelled(true);
 				}
 			}
 		}
